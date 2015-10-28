6db8e2f7a4d069134b1fb5b825b22f52a8103bc5
Russell Simpkins <russell.simpkins@nytimes.com>
2015-10-28 17:51:40 -0400

    changed the getopts to be bsd compat and added 2 new options

--- scripts/sync-getopt.sh
+++ scripts/sync-getopt.sh
@@ -2,25 +2,20 @@
 GETOPT_COMPATIBLE=1
 ########################################################################
 #
-# This has our options for -esome  --env some -- type usage
+# This has our command line options
 ########################################################################
 usage() {
     echo "${0} required parameters"
-    echo "-d --direction	: specify the sync direction up or down allowed"
-    echo "-e --env    	: specify the environment"
+    echo "-d	: specify the sync direction up or down allowed"
+    echo "-e    	: specify the environment"
+    echo "-t	: specify the team"
+    echo "-a :	specify the app"
     exit 1
 }
 
-# deal with getopt on mac osx
-if [ -f /usr/local/opt/gnu-getopt/bin/getopt ]; then
-    getopt=/usr/local/opt/gnu-getopt/bin/getopt
-else
-    getopt=getopt
-fi
-
-TEMP=$($getopt -o e:d: --long env:,direction: -n $0 -- "$@")
+TEMP=$(getopt a:e:d:t: $@)
 if [ $? -ne 0 ]; then
-    echo "Parser problem. Make sure you have the right getopt installed."
+    echo "Parser problem maybe."
     usage
 fi
 
@@ -29,17 +24,23 @@ eval set -- "$TEMP"
 
 while true ; do
     case "$1" in
-        -d|--direction)
+        -a)
+            app=$2
+            shift 2
+            ;;
+        -d)
             direction=$2
             shift 2
             ;;
-        -e|--env)
+        -e)
             env=$2
+            shift 2
+            ;;
+        -t)
+            team=$2
             shift 2
             ;;
         --) shift ; break ;;
         *) echo "Big fail. Check that getopt matches your case statement! $1 $2" ; exit 1 ;;
     esac
 done
-
-
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,19 +7,35 @@ path=$( dirname "${BASH_SOURCE[0]}" )
 
 source ${path}/sync-getopt.sh
 
-if [[ "$env" == "" || "$direction" =~ /(up|down)/ ]]; then
+if [[ "$env" == "" || "$direction" =~ /(up|down)/ || "$team" == "" || "$app" == "" ]]; then
     echo "Missing required parameter(s) $direction $env"
     usage
     exit 1
 fi
 
-if [ ! -d "./remote/du/${env}" ]; then
-    mkdir -p "./remote/du/${env}"
+if [ -f "${path}/.aws_creds" ]; then
+    source "${path}/.aws_creds"
+fi
+
+if [[ "" == "${AWS_ACCESS_KEY_ID}" || "" == "${AWS_SECRET_ACCESS_KEY}" ]]; then
+    echo "[ERROR] Missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY. You can add a ${path}/.aws_creds file to load the creds."
+    exit 1
+fi
+
+if [ ! -d "./remote/${team}/${env}" ]; then
+    mkdir -p "./remote/${team}/${env}"
 fi
 
 if [ "${direction}"  == "up" ]; then
-    aws s3 sync ./remote/du/${env}/ s3://infrastructure-deploy-nyt-net/du/${env}/
+
+    # our platform repo does the job of updating the yum repo, which happens to live in the CI folder.
+    # so, we can't sync with --delete
+    if [ "${app}" == "ci" ]; then
+        AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws s3 sync ./remote/${team}/${env}/${app}/ s3://infrastructure-deploy-nyt-net/${team}/${env}/${app}/
+    else
+        AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws s3 sync --delete ./remote/${team}/${env}/${app}/ s3://infrastructure-deploy-nyt-net/${team}/${env}/${app}/
+    fi
 else
-    echo aws s3 sync s3://infrastructure-deploy-nyt-net/du/${env}/ ./remote/du/${env}/
-    aws s3 sync s3://infrastructure-deploy-nyt-net/du/${env}/ ./remote/du/${env}/
+    echo aws s3 sync s3://infrastructure-deploy-nyt-net/${team}/${env}/ ./remote/${team}/${env}/
+    aws s3 sync s3://infrastructure-deploy-nyt-net/${team}/${env}/ ./remote/${team}/${env}/
 fi
